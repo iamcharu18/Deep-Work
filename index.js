54775ef34e2e0c42c32687aae06d66c194ecf8e3
Sobhan Sai Kuriti <sobhansaikuriti03@gmail.com>
2023-04-07 11:46:20 +0530

    Email Fix

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post("/sendmail", function (req, res) {
             });
             const mailOptions = {
                 from: "contact@deepworkco.in",
-                to: "sobhansaikuriti03@gmail.com",
+                to: "contact@deepworkco.in",
                 subject: `New Contact Sent from website`,
                 html: `<h2>Business Type : ${businessType}</h2><p>Name : ${name}</p><p>Email : ${email}</p><p>Mobile : ${phone}</p>`
             };
@@ -168,7 +168,7 @@ app.post("/sendmail-2", function (req, res) {
             });
             const mailOptions = {
                 from: "contact@deepworkco.in",
-                to: "sobhansaikuriti03@gmail.com",
+                to: "contact@deepworkco.in",
                 subject: subject,
                 html: `<h2>Page : ${subject}</h2><p>Name : ${name}</p><p>Email : ${email}</p><p>Mobile : ${phone}</p>`
             };
